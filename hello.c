5d23af7a74bd655ce0b38bbeedc46a4c303c252b
xiezhaohai <18251173372@163.com>
2015-10-24 12:42:42 -0500

    This is add hello.c  helle.h hello main.c

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void helloworld(void) 
+{
+	printf("hello world!\n");
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include "hello.h"
+
+int main(int argc, char **argv) 
+{
+	helloworld();
+	
+	return 0;
+}
